4d6a84ba8ba7e9b0b54a1154895cde3a9aca8034
amit152004singh <134773531+amit152004singh@users.noreply.github.com>
2024-06-22 17:11:28 +0530

    some minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Blogs from './components/Blogs';
-// import Contact from './components/Contact';
-// import About from './components/About';
-// import NavBar from './components/NavBar';
-// import Home from './components/Home';
-// import Events from './components/Events';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Blogs from './components/Blogs';
+import Contact from './components/Contact';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Events from './components/Events';
 
 
 
-// const Layout = ({ children }) => (
-//   <>
-//     <NavBar />
-//     {children}
-//     <Events/>
-//   </>
-// );
+const Layout = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+    <Events/>
+  </>
+);
 
-// const App = () => (
-//   <Router>
-//     <Layout>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/blogs" element={<Blogs />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//     </Layout>
-//   </Router>
-// );
+const App = () => (
+  <Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Layout>
+  </Router>
+);
 
-// export default App;
+export default App;
